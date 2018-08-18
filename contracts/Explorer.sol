26b6acfd0c5b5b2f7d32902b7c78a85f9bd84d42
Sumitra Lok <sumitra.lok@gmail.com>
2018-08-19 04:11:55 +0800

    Product

---
+++ contracts/Explorer.sol
@@ -0,0 +1,55 @@
+pragma solidity ^0.4.23;
+
+contract Explorer {
+    
+    address public owner;
+
+    struct Product {
+        string name;
+        string source_name;
+        address[] ingredientsArray;
+    }
+
+    mapping (address => Product) public products;
+
+    struct Ingredient {
+        string name;
+    }
+
+    mapping (address => Ingredient) public ingredients;
+
+    constructor() public {
+        owner = msg.sender;
+    }
+    
+    function addProduct(address _product_address, string _name, string _kings_name) public {
+        // creating a temp struct first
+        Product memory newProduct;
+        newProduct.name = _name;
+        newProduct.source_name = _kings_name;
+
+        // saving struct into the `products` mapping
+        products[_product_address] = newProduct;
+    }
+
+    function addIngredient(address _ingredients_address, address _product_address, string _name) public {
+        // creating a temp struct first
+        Ingredient memory newIngredient;
+        newIngredient.name = _name;
+
+        // saving struct into the `ingredients` mapping
+        ingredients[_ingredients_address] = newIngredient;
+
+        Product storage _product = products[_product_address];
+        _product.ingredientsArray.push(_ingredients_address);
+    }
+
+    function getDestrictLength(address _product_address) public view returns (uint256) {
+        return products[_product_address].ingredientsArray.length;
+    }
+
+    function getDestrict(address _product_address, uint256 _index) public view returns (address) {
+        return products[_product_address].ingredientsArray[_index];
+    }
+
+}
---
+++ migrations/2_explorer.js
@@ -0,0 +1,5 @@
+var my = artifacts.require("./Explorer.sol");
+
+module.exports = function(deployer) {
+  deployer.deploy(my);
+};
